4796da6bf7ff938f1465fc0f065227e51641d5a1
Henrik Renlund <henrikrenlund@gmail.com>
2023-09-07 16:31:48 +0200

    code for 'align' added (but no export yet)

--- R/order-as.R
+++ R/order-as.R
@@ -1,3 +1,54 @@
+## order_as / order_as_list should possibly be replaced with align
+align <- function(x, template = NULL, group = NULL, all = TRUE, outgroup = ".Other"){
+    if(length(x) == 0){
+        warning("zero length input makes no sense")
+        return(as.list(NULL))
+    }
+    if(!is.null(group) & !is.null(template)){
+        if(length(group) != length(template)){
+            stop("template and group of the same length, please")}
+    }
+    if(is.null(template)) template = sort(x)
+    m <- match(x, template)
+    distinct_m <- sort(unique(na.omit(m)))
+    order <- rep(NA_integer_, length(x))
+    dummy <- 0L
+    for(d in distinct_m){
+        i <- which(x %in% x[which(d==m)][1])
+        n <- length(i)
+        order[dummy + 1:n] <- i
+        dummy <- dummy + n
+    }
+    if(any(is.na(m))){
+        if(all){
+            order[which(is.na(order))] <- which(is.na(m))
+        } else {
+            order <- order[!is.na(order)]
+        }
+    }
+    z <- data.frame(x = x[order])
+    if(is.null(group)){
+        z$group <- outgroup
+        list(order = order,
+             sorted = z,
+             group.rle = list(lengths = nrow(z),
+                              values = outgroup))
+    } else {
+        tg <- data.frame(template = template, group = group)
+        s <- merge(x = z, y = tg, all.x = TRUE,
+                   by.x = "x", by.y = "template", sort = FALSE)
+        if(all){
+            s$group[is.na(s$group)] <- outgroup
+        }
+        Rle <- rle(s$group)
+        class(Rle) <- "list"
+        list(order = order,
+             sorted = s,
+             group.rle = Rle)
+    }
+}
+
+
 ##' ordering of vector
 ##'
 ##' order a given vector, which may contain duplicates, according to the
